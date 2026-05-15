7388c27c04fb339f6c7a4ae223298ba1b5ebfc22
anonimatogb <gabrielcavalcante22@outlook.com>
2026-05-15 07:38:48 -0300

    apenas pastas

---
+++ db/database.php
@@ -0,0 +1,14 @@
+<?php
+
+$host="localhost";
+$db="escola";
+$user="root";
+$pass="";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    // Configura o modo de erro do PDO para exceção
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro ao conectar ao banco de dados: " . $e->getMessage());
+}
