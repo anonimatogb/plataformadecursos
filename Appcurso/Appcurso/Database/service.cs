c7c62a6cd9a593caf2cb29d696449555c394e4d7
Zucolin <vinibr555@gmail.com>
2026-05-20 09:12:03 -0300

    Appcursos

---
+++ Appcurso/Appcurso/Database/service.cs
@@ -0,0 +1,45 @@
+﻿using MySqlConnector;
+
+namespace Appcurso.Database
+{
+    public class Service
+    {
+        private readonly string _connectionString;
+
+        public Service()
+        {
+        }
+
+        public Service(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void InserirUsuario(string nome, int idade)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            connection.OpenAsync();
+
+            using var cmd = new MySqlCommand(
+                "INSERT INTO usuarios (nome, idade) VALUES (@nome, @idade)",
+                connection);
+
+            cmd.Parameters.AddWithValue("@nome", nome);
+            cmd.Parameters.AddWithValue("@idade", idade);
+
+            cmd.ExecuteNonQuery();
+        }
+        public bool VerificarUsuario(string email, string senha)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            connection.OpenAsync();
+            using var cmd = new MySqlCommand(
+                "SELECT COUNT(*) FROM usuarios WHERE email = @email AND senha = @senha",
+                connection);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@senha", senha);
+            long count = (long)cmd.ExecuteScalar();
+            return count > 0;
+        }
+    }
+}
---
+++ Appcurso/Appcurso/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Appcurso
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            bool existe = new Database.Service()
+                .VerificarUsuario(txt_email.Text, txt_senha.Text);
+
+            if (.VerificarUsuario == true)
+            {
+                await DisplayAlert("Sucesso", "Login bem-sucedido!", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Erro", "Email ou senha incorretos.", "OK");
+            }
+        }
+    }
+}
