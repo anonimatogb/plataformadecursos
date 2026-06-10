76755681c03040869f402f156302ecb51349d396
anonimatogb <gabrielcavalcante22@outlook.com>
2026-06-10 15:23:01 -0300

    db

--- Web/db_backup/plataformacursos.sql
+++ Web/db_backup/plataformacursos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10/06/2026 às 15:27
+-- Tempo de geração: 10/06/2026 às 20:12
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -24,52 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `cursos`
---
-
-CREATE TABLE `cursos` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL,
-  `fotocapa` longblob NOT NULL,
-  `descricao` varchar(255) NOT NULL,
-  `professor` int(11) NOT NULL,
-  `carga_horaria` int(11) NOT NULL,
-  `certificado` longblob NOT NULL,
-  `ativo` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Despejando dados para a tabela `cursos`
---
-
-
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `matriculas`
---
-
-CREATE TABLE `matriculas` (
-  `id` int(11) NOT NULL,
-  `aluno_id` int(11) NOT NULL,
-  `cursos_id` int(11) NOT NULL,
-  `professor_id` int(11) NOT NULL,
-  `data_matricula` date NOT NULL,
-  `concluido` int(11) NOT NULL,
-  `ativo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Despejando dados para a tabela `matriculas`
---
-
-INSERT INTO `matriculas` (`id`, `aluno_id`, `cursos_id`, `professor_id`, `data_matricula`, `concluido`, `ativo`) VALUES
-(1, 2, 8, 2, '2026-06-10', 1, 1);
-
--- --------------------------------------------------------
-
---
 -- Estrutura para tabela `modulo`
 --
 
@@ -77,50 +31,14 @@ CREATE TABLE `modulo` (
   `id` int(11) NOT NULL,
   `titulo` varchar(255) NOT NULL,
   `cursos_id` int(11) NOT NULL,
-  `video` longblob NOT NULL,
+  `video` varchar(255) NOT NULL,
   `professor` int(11) NOT NULL,
   `ativo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL,
-  `cargo` varchar(255) NOT NULL,
-  `foto_perfil` longblob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Despejando dados para a tabela `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `cargo`, `foto_perfil`) VALUES
-(1, 'Gabriel', 'gabrielcavalcante22@outlook.com', '123', 'admin', NULL);
 --
 -- Índices para tabelas despejadas
 --
-
---
--- Índices de tabela `cursos`
---
-ALTER TABLE `cursos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices de tabela `matriculas`
---
-ALTER TABLE `matriculas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_professor_matricula` (`professor_id`),
-  ADD KEY `fk_aluno_matricula` (`aluno_id`),
-  ADD KEY `fk_cursos_matricula` (`cursos_id`);
 
 --
 -- Índices de tabela `modulo`
@@ -131,56 +49,24 @@ ALTER TABLE `modulo`
   ADD KEY `fk_professor_modulo` (`professor`);
 
 --
--- Índices de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT para tabelas despejadas
 --
-
---
--- AUTO_INCREMENT de tabela `cursos`
---
-ALTER TABLE `cursos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
-
---
--- AUTO_INCREMENT de tabela `matriculas`
---
-ALTER TABLE `matriculas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 
 --
 -- AUTO_INCREMENT de tabela `modulo`
 --
 ALTER TABLE `modulo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
-
---
--- AUTO_INCREMENT de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Restrições para tabelas despejadas
 --
 
 --
--- Restrições para tabelas `matriculas`
---
-ALTER TABLE `matriculas`
-  ADD CONSTRAINT `fk_aluno_matricula` FOREIGN KEY (`aluno_id`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_cursos_matricula` FOREIGN KEY (`cursos_id`) REFERENCES `cursos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_professor_matricula` FOREIGN KEY (`professor_id`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
 -- Restrições para tabelas `modulo`
 --
 ALTER TABLE `modulo`
-  ADD CONSTRAINT `fk_modulo_cursos` FOREIGN KEY (`cursos_id`) REFERENCES `cursos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_curso_modulo` FOREIGN KEY (`cursos_id`) REFERENCES `cursos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_professor_modulo` FOREIGN KEY (`professor`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
